c1a88d05fc542db048250c524979cf9924d1d226
webkonglong <anmingzhe@me.com>
2021-05-30 20:23:50 +0800

    [feat]

---
+++ src/pages/home/components/tip.tsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+import Icon from 'components/icon'
+import { TipProps } from '../type'
+
+const Tip = styled.header`
+  background-color: #fff8f9;
+  border-color: #ffeaef;
+  color: #ff3465;
+  height: 48px;
+  font-size: 14px;
+  padding: 0 24px;
+  line-height: 48px;
+
+  .tip-close {
+    font-size: 14px;
+    float: right;
+    position: relative;
+    top: 17px;
+    cursor: pointer;
+  }
+`
+
+export default ({close}: TipProps) => {
+  return (
+    <Tip>
+      本站大部分空投需要科学上网，请将本站添加到浏览器书签。
+      <Icon onClick={() => {close()}} className="tip-close" type="funder-guanbi" />
+    </Tip>
+  );
+}
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,8 +1,14 @@
+import {useState} from 'react'
 import Header from './components/header'
+import Tip from './components/tip'
 
 export default () => {
+  const [isShow, toggle] = useState<boolean>(true)
   return (
     <>
+      {isShow && <Tip close={() => {
+        toggle(false)
+      }} />}
       <Header />
     </>
   );
